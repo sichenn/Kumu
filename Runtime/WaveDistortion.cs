b83035a6f9b2b01f5a6e25dde38c410749ce7351
RetroSpecter <InsertNameHere4897@gmail.com>
2021-04-30 18:32:05 -0700

    wavy distortion shader added

---
+++ Runtime/WaveDistortion.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Kumu
+{
+    [System.Serializable]
+    [PostProcess(typeof(WaveDistortionRenderer), PostProcessEvent.AfterStack, "Kumu/WaveDistortion")]
+    public sealed class WaveDistortion : PostProcessEffectSettings
+    {
+        public FloatParameter period = new FloatParameter();
+        public FloatParameter speed = new FloatParameter();
+        public FloatParameter posterization = new FloatParameter();
+        public FloatParameter amplitude = new FloatParameter();
+    }
+
+    sealed class WaveDistortionRenderer : PostProcessEffectRenderer<WaveDistortion>
+    {
+        static class ShaderIDs
+        {
+            internal static readonly int Period = Shader.PropertyToID("_Period");
+            internal static readonly int Speed = Shader.PropertyToID("_Speed");
+            internal static readonly int Posterization = Shader.PropertyToID("_Poisterization"); 
+            internal static readonly int Amplitude = Shader.PropertyToID("_Amplitude");
+        }
+
+        public override void Init()
+        {
+            // though the name is fitToScreenRatio we actually fit it to the camera's ratio
+        }
+
+        public override void Render(PostProcessRenderContext context)
+        {
+
+            var sheet = context.propertySheets.Get(Shader.Find("Hidden/Kumu/WaveDistortion"));
+
+            sheet.properties.SetFloat(ShaderIDs.Period, settings.period);
+            sheet.properties.SetFloat(ShaderIDs.Speed, settings.speed);
+            sheet.properties.SetFloat(ShaderIDs.Amplitude, settings.amplitude);
+            sheet.properties.SetFloat(ShaderIDs.Posterization, settings.posterization);
+
+            var cmd = context.command;
+            cmd.BeginSample("WaveDistortion");
+            cmd.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+            cmd.EndSample("WaveDistortion");
+
+        }
+    }
+}
---
+++ Shaders/WaveDistortion.shader
@@ -0,0 +1,47 @@
+﻿Shader "Hidden/Kumu/WaveDistortion"
+{
+	HLSLINCLUDE
+	// for Unity 2018.2 or Newer
+		#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+	// for below Unity 2018.2 
+		// #include "PostProcessing/Shaders/StdLib.hlsl"
+		#include "PSBlend.hlsl"
+
+
+        TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+        TEXTURE2D_SAMPLER2D(_BlendTex, sampler_BlendTex);
+
+		half _Period;
+		half _Poisterization;
+		half _Speed;
+		half _Amplitude;
+
+		float Posterize(float In, float Steps)
+		{
+			return floor(In / (1 / Steps)) * (1 / Steps);
+		}
+
+		float4 Frag(VaryingsDefault i) : SV_Target
+		{
+			float distort = sin(_Time.x * -10 * _Speed + i.texcoord.y * _Period); 
+			distort = Posterize(distort, _Poisterization) * 0.01f * _Amplitude * i.texcoord.y;
+
+			float2 uv = float2(i.texcoord.x + distort, i.texcoord.y);
+			float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv);
+
+			return color;
+        }
+	ENDHLSL
+	SubShader
+	{
+		ZWrite Off Cull Off ZTest Always
+
+		Pass
+		{
+			HLSLPROGRAM
+				#pragma vertex VertDefault
+				#pragma fragment Frag
+			ENDHLSL
+		}
+	}
+}
